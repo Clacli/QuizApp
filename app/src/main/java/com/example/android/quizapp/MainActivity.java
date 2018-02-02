2a4cf2f8488eafe918e07435e0d78851f813997e
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-02-02 15:57:12 +0100

    Replace xlm onClick attribute with onClickListener

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,23 +3,35 @@ package com.example.android.quizapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
+    /**
+     * onClick method diplay a message toast on the screen (and a test message) when submit button is clicked
+     */
+    private OnClickListener mListener = new OnClickListener() {
+        public void onClick(View v) {
+            displayGrading("Your current score is " + score);
+            Toast.makeText(getApplicationContext(), "Your current score is " + score, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    // Create an anonymous implementation of OnClickListener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void gradeAnswersAndDisplayScore(View view) {
-        displayGrading("Your current score is " + score);
-        // Show test message now(later message which accurately displays the results of the quiz) as a toast.
-        Toast.makeText(this, "Your current score is " + score, Toast.LENGTH_LONG).show();
+        //Capture submit button from layout
+        final Button submitButton = (Button) findViewById(R.id.submit_button);
+        //Register the onClick listener with the implementated button
+        submitButton.setOnClickListener(mListener);
     }
 
     /**
