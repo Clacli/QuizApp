d1c592936564acbe163bc7c8a3c665c696e31c60
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-02-04 17:26:12 +0100

    Add the remaining quiz questions views and grading logic

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -20,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
     private OnClickListener mListener = new OnClickListener() {
         public void onClick(View v) {
+            calculateScoreForBilberry();
+            Log.v("MainActivity", "score is: " + score);
+            calulateScoreForAsteraceae();
+            Log.v("MainActivity", "score is: " + score);
+            calculateScoreForPlantago();
+            Log.v("MainActivity", "score is: " + score);
+            calculateScoreForOleaceae();
+            Log.v("MainActivity", "score is: " + score);
             calulateScoreForRosaceae();
             Log.v("MainActivity", "score is: " + score);
             calculateScoreForOrange();
@@ -30,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
             Log.v("MainActivity", "score is: " + score);
             calculateScoreForCrocus();
             Log.v("MainActivity", "score is: " + score);
-            Toast.makeText(getApplicationContext(), "Your current score is " + score, Toast.LENGTH_SHORT).show();
+            calculateScoreForLythrum();
+            Log.v("MainActivity", "score is: " + score);
+            Toast.makeText(getApplicationContext(), "Your score is " + score, Toast.LENGTH_SHORT).show();
             displayGrading("Your current score is " + score);
         }
     };
@@ -49,7 +59,89 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method calculate the score for the Apiaceae quiz
+     * This method calculate the score for the bilberry quiz
+     *
+     * @return score of the quiz
+     */
+    private int calculateScoreForBilberry() {
+        // Figure out if the user has checked yes answer
+        RadioButton noOption = findViewById(R.id.no_radio_button);
+        boolean isNoChecked = noOption.isChecked();
+        if (isNoChecked) {
+            score = score + 1;
+        }
+        return score;
+    }
+
+    /**
+     * This method calculate the score for the Oleaceae quiz
+     *
+     * @return score of the quiz
+     */
+    private int calculateScoreForOleaceae() {
+        // Figure out if the user has checked Oleaceae answer
+        RadioButton oleaceaeOption = findViewById(R.id.oleaceae_radio_button);
+        boolean isOleaceaeChecked = oleaceaeOption.isChecked();
+        if (isOleaceaeChecked) {
+            score = score + 1;
+        }
+        return score;
+    }
+
+    /**
+     * This method calculate the score for the Achillea quiz
+     *
+     * @return score of the quiz
+     */
+    private int calculateScoreForAhillea() {
+        // Figure out if the user has checked Asteraceae answer
+        RadioButton asteraceaeOption = findViewById(R.id.asteraceae_radio_button);
+        boolean isAsteraceaeChecked = asteraceaeOption.isChecked();
+        if (isAsteraceaeChecked) {
+            score = score + 1;
+        }
+        return score;
+    }
+
+    /**
+     * This method calculate the score for the Lythrum quiz
+     *
+     * @return score of the quiz
+     */
+    private int calculateScoreForLythrum() {
+        // Figure out if the user has checked yes answer
+        RadioButton yesOption = findViewById(R.id.yes_radio_button);
+        boolean isYesChecked = yesOption.isChecked();
+        if (isYesChecked) {
+            score = score + 1;
+        }
+        return score;
+    }
+
+    /**
+     * This method calculate the score for the Rosaceae quiz
+     *
+     * @return score of the quiz
+     */
+    private int calulateScoreForAsteraceae() {                    //TODO Extract strings, translate, pics, header and landscape mode
+        // Figure out if the user has checked carrot answer
+        CheckBox carrotOption = findViewById(R.id.carrot_checkbox);
+        boolean isCarrotChecked = carrotOption.isChecked();
+        // Figure out if the user has checked daisy answer
+        CheckBox daisyOption = findViewById(R.id.daisy_checkbox);
+        boolean isDaisyChecked = daisyOption.isChecked();
+        //Figure it out if the user has checked calendula answer
+        CheckBox calendulaOption = findViewById(R.id.calendula_checkbox);
+        boolean isCalendulaChecked = calendulaOption.isChecked();
+        //Check if answer is correct
+        if (!isCarrotChecked && isDaisyChecked && isCalendulaChecked) {
+            score = score + 1;
+        }
+        return score;
+    }
+
+    /**
+     * This method calculate the score for the Rosaceae quiz
      *
      * @return score of the quiz
      */
@@ -65,22 +157,6 @@ public class MainActivity extends AppCompatActivity {
         boolean isHawthornChecked = hawthornOption.isChecked();
         //Check if answer is correct
         if (isAppleChecked && !isCornelianCherryChecked && isHawthornChecked) {
-            score = score + 1;
-        }
-        return score;
-    }
-
-
-    /**
-     * This method calculate the score for the Achillea quiz
-     *
-     * @return score of the quiz
-     */
-    private int calculateScoreForAhillea() {
-        // Figure out if the user has checked Asteraceae answer
-        RadioButton asteraceaeOption = findViewById(R.id.asteraceae_radio_button);
-        boolean isAsteraceaeChecked = asteraceaeOption.isChecked();
-        if (isAsteraceaeChecked) {
             score = score + 1;
         }
         return score;
@@ -109,15 +185,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method calculate the score for the Plantago quiz
+     *
+     * @return score of the quiz
+     */
+    private int calculateScoreForPlantago() {
+        //Find the answer
+        EditText plantagoView = (EditText) findViewById(R.id.plantago_edit);
+        // Set keyboard button Done
+        plantagoView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        String orangeAnswer = plantagoView.getText().toString();
+        //Check if answer is correct to increase score by 1
+        if (orangeAnswer.compareTo("Spike") == 0) {
+            score = score + 1;
+        }
+        return score;
+    }
+
+    /**
      * This method calculate the score for the Orange quiz
      *
      * @return score of the quiz
      */
     private int calculateScoreForOrange() {
-        //Find the user's name
+        //Find the answer
         EditText orangeView = (EditText) findViewById(R.id.orange_edit);
+        // Set keyboard button Done
         orangeView.setImeOptions(EditorInfo.IME_ACTION_DONE);
         String orangeAnswer = orangeView.getText().toString();
+        //Check if answer is correct to increase score by 1
         if (orangeAnswer.compareTo("Citrus") == 0) {
             score = score + 1;
         }
@@ -130,9 +226,12 @@ public class MainActivity extends AppCompatActivity {
      * @return score of the quiz
      */
     private int calculateScoreForCrocus() {
-        //Find the user's name
+        //Find the answer
         EditText crocusView = (EditText) findViewById(R.id.crocus_edit);
+        // Set keyboard button Done
+        crocusView.setImeOptions(EditorInfo.IME_ACTION_DONE);
         String crocusAnswer = crocusView.getText().toString();
+        //Check if answer is correct to increase score by 1
         if (crocusAnswer.compareTo("Saffron") == 0) {
             score = score + 1;
         }
